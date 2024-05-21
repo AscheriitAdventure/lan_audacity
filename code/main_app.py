2c9547af77dbf28e2c3eb2d3f6a536955d21bb6c
AscheriitAdventure <94352743+AscheriitAdventure@users.noreply.github.com>
2024-05-21 16:53:27 +0200

    workspace 16:53

--- code/main_app.py
+++ code/main_app.py
@@ -9,7 +9,9 @@ import csv
 import configparser
 import string
 import qtawesome as qta
-from PyQt5.QtWidgets import QMainWindow, QStatusBar
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 
 
 class SwitchFile:
@@ -142,20 +144,51 @@ class ShortcutApp:
     def __init__(self, file_manager: ConfigurationFile):
         self.data_manager = file_manager.data
 
-    def get_shortcut(self, key: str):
+    def get_shortcut(self, key: str) -> str:
         for shortcut in self.data_manager:
-            if shortcut["name_action"] == key:
-                return shortcut["keyshortcut"]
+            if shortcut["name"] == key:
+                return shortcut["keys"]
 
 
 class IconsApp:
     def __init__(self, file_manager: ConfigurationFile):
         self.data_manager = file_manager.data
-    
-    def get_icon(self, key: str) -> any:
-        for icon in self.data_manager:
-            if icon['name'] == key:
-                return qta.icon(icon["platform_and_name"])
+
+    def get_icon(self, key: str) -> QIcon:
+        try:
+            for icon in self.data_manager:
+                if icon['name'] == key:
+                    options = icon["options"] if len(icon["options"]) >= 1 else None
+                    ico_obj = qta.icon(*icon["platform_and_name"], options=options)
+                    logging.debug(ico_obj)
+                    if ico_obj is not None:
+                        return ico_obj
+        except ValueError as e:
+            logger.error(e)
+        except Exception as e:
+            logger.error(f"Une erreur inattendue s'est produite lors de la récupération de l'icône '{key}': {e}")
+        return QIcon()
+
+
+class MenuBarApp:
+    def __init__(self, file_manager: ConfigurationFile):
+        self.data_manager = file_manager.data
+
+    def get_menuName(self, key: str) -> str:
+        for menu_obj in self.data_manager:
+            if menu_obj["name"] == key:
+                return menu_obj["menu"]
+
+    def get_one_menu(self, key: str) -> dict:
+        for menu_obj in self.data_manager:
+            if menu_obj["name"] == key:
+                return menu_obj
+
+    def get_one_action(self, key_menu: str, key_action) -> dict:
+        menu = self.get_one_menu(key_menu)
+        for action in menu["actions"]:
+            if action["name_low"] == key_action:
+                return action
 
 
 class MainApp(QMainWindow):
@@ -166,21 +199,28 @@ class MainApp(QMainWindow):
         # Data Language Manager
         self.langManager = LanguageApp(
             ConfigurationFile(
-                f"{current_dir()}/{self.softwareManager.data['software']['configuration_translate']['path']}"
+                f"{current_dir()}/{self.softwareManager.data['software']['conf']['translate_app']['path']}"
             )
         )
         # Data Shortcut Manager
         self.shortcutManager = ShortcutApp(
             ConfigurationFile(
-                f"{current_dir()}/{self.softwareManager.data['software']['configuration_key_shortcuts']['path']}"
+                f"{current_dir()}/{self.softwareManager.data['software']['conf']['shortcuts_app']['path']}"
             )
         )
         # Data Icon Manager
         self.iconsManager = IconsApp(
             ConfigurationFile(
-                f"{current_dir()}/{self.softwareManager.data['software']['configuration_icons']['path']}"
+                f"{current_dir()}/{self.softwareManager.data['software']['conf']['icons_app']['path']}"
             )
         )
+        # Data MenuBar Manager
+        self.menuBarManger = MenuBarApp(
+            ConfigurationFile(
+                f"{current_dir()}/{self.softwareManager.data['software']['conf']['navBar_app']['path']}"
+            )
+        )
+
         # Set the Window title
         self.setWindowTitle(self.softwareManager.data['system']['name'])
         # Set the Window icon
@@ -188,23 +228,81 @@ class MainApp(QMainWindow):
         # Center the window
         self.centerWindow()
         # Set the navBar
-        self.menuBar()
+        self.initUI_menu()
         # Set the Status bar
         self.status_bar = QStatusBar()
         self.setStatusBar(self.status_bar)
-        # Set the window
+
+        # Setting the GUI Information
+        self.initUI_central()
+
+        self.show()
 
     def centerWindow(self):
+        # Obtenir la géométrie de l'écran
+        screen_geometry = QApplication.desktop().screenGeometry()
+
+        # Obtenir la géométrie de la fenêtre
+        window_geometry = self.geometry()
+
+        # Calculer la position pour centrer la fenêtre
+        x = (screen_geometry.width() - window_geometry.width()) // 2
+        y = (screen_geometry.height() - window_geometry.height()) // 2
+
+        # Déplacer la fenêtre
+        self.move(x, y)
+
+    def initUI_menu(self):
+        # Add toolbars
+        infobar = QToolBar(self)
+        # Set the toolbar to a fixed top position
+        self.addToolBar(Qt.TopToolBarArea, infobar)
+        # Add a Spacer
+        spacerI = QWidget()
+        spacerI.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        infobar.addWidget(spacerI)
+
+        toolsbar = QToolBar(self)
+        # Set the toolbar to a fixed left position
+        self.addToolBar(Qt.LeftToolBarArea, toolsbar)
+
+        commandLine = QLineEdit("Lan Audacity")
+        commandLine.setAlignment(Qt.AlignCenter)
+        commandLine.setClearButtonEnabled(True)
+        commandLine.setAcceptDrops(True)
+        commandLine.setDragEnabled(True)
+        commandLine.setMinimumWidth(75)
+        commandLine.setMaximumWidth(250)
+        # Add commandLine to infobar
+        infobar.addWidget(commandLine)
+
+        for menu in self.menuBarManger.data_manager:
+            menu_obj = self.menuBar().addMenu(menu['menu'])
+            for action in menu['actions']:
+                act_obj = QAction(action['name'], self)
+                act_obj.setIcon(self.iconsManager.get_icon(action['icon_name']))
+                act_obj.setShortcut(self.shortcutManager.get_shortcut(action['shortcut_name']))
+                act_obj.setStatusTip(action['status_tip'])
+                # act_obj.triggered.connect(action['trigger'])
+                menu_obj.addAction(act_obj)
+                if action['name_low'] == "save_project":
+                    menu_obj.addSeparator()
+                elif action['name_low'] == "exit":
+                    menu_obj.addSeparator()
+
+            # Add Action to the infobar
+            toggle_pSideBar = QAction(qta.icon("fa5s.window-maximize", rotated=270), "Toggle Primary Side Bar", self)
+            toggle_pSideBar.setShortcut("Ctrl+B")
+            toggle_pSideBar.setStatusTip("Toggle Primary Side Bar")
+            # toggle_pSideBar.triggered.connect(self.toggle_primary_side_bar)  # Define the function to be triggered
+
+    def initUI_central(self):
         pass
-    
-    def initUI(self):
-        pass     
 
 
 if __name__ == "__main__":
     softw_manager = ConfigurationFile(current_dir() + "\\conf\\config_app.yaml")
-    print(softw_manager.abs_path)
-    path_log = f"{current_dir()}/{softw_manager.data['software']['configuration_logs']['path']}"
+    path_log = f"{current_dir()}/{softw_manager.data['software']['conf']['log_app']['path']}"
     logs_manager = ConfigurationFile(path_log)
 
     logging.config.dictConfig(logs_manager.data)
@@ -224,4 +322,4 @@ if __name__ == "__main__":
     main_window = MainApp(softw_manager)
     main_window.show()
 
-    sys.exit(app.exec_())    
+    sys.exit(app.exec_())
