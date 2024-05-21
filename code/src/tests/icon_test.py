6ddeec110e2b7b85bd9a282169f9391349f07ddf
AscheriitAdventure <94352743+AscheriitAdventure@users.noreply.github.com>
2024-05-21 22:28:15 +0200

    workspace 22:28

---
+++ code/src/tests/icon_test.py
@@ -0,0 +1,30 @@
+import json
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+import qtawesome as qta
+
+
+class IconTest(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Icon Test")
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.load_icon()
+
+    def load_icon(self):
+        with open(
+            "/Users/yuna/Documents/GitHub/lan_audacity/code/data/icon_list.json"
+        ) as f:
+            data = json.load(f)
+        for icon in data:
+            button = QPushButton(icon["name"])
+            icon = qta.icon(*icon["platform_and_name"])
+            button.setIcon(icon)
+            self.layout.addWidget(button)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = IconTest()
+    window.show()
+    app.exec_()
