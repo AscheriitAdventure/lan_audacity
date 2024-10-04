a2d96f36baba19e8a80b747c740d640808356e73
AscheriitAdventure <94352743+AscheriitAdventure@users.noreply.github.com>
2024-10-04 17:29:22 +0200

    8889

---
+++ code/data/chatgpt/obj-04102024.py
@@ -0,0 +1,4 @@
+"""Cette Fonction test a pour but de tester l'installation des bases de données avec Docker-compose"""
+
+VAR_DOCKER_COMPOSE = "docker-compose -f /home/etudiant/PycharmProjects/Projet-DevOps/docker-compose.yml up -d"
+
