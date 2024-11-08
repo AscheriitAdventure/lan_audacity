acbcf6b483098e4b2f460da14f34a3c781868359
AscheriitAdventure <94352743+AscheriitAdventure@users.noreply.github.com>
2024-11-08 09:21:10 +0100

    5566

---
+++ code/src/components/card/cl_card.py
@@ -0,0 +1,10 @@
+"""
+    Card component:
+    - "Card" est un composant qui permet d'afficher des informations sous forme de carte.
+    - "Card" est composé de 3 parties:
+        - "CardHeader": La partie supérieure de la carte qui contient le titre de la carte et un icone.
+        - "CardBody": La partie centrale de la carte qui contient les informations à afficher.
+        - "CardFooter": La partie inférieure de la carte qui contient les actions à effectuer sur la carte.
+    - "Card" est un composant générique qui peut être utilisé dans plusieurs contextes.
+    - "Card" est un composant qui est 
+"""
