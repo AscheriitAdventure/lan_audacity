430bbaa593670e784b17cda82c24771089a0c1df
AscheriitAdventure <94352743+AscheriitAdventure@users.noreply.github.com>
2025-03-27 12:01:14 +0100

    1219

--- src/views/widgets/__init__.py
+++ src/views/widgets/__init__.py
@@ -1,10 +1,10 @@
-from .widget_field import WidgetField
-from .card import Card
-from .card_image import CardImage
-from .card_hearder import CardHeader
-from .card_footer import CardFooter
+from .card.widget_field import WidgetField
+from .card.card import Card
+from .card.card_image import CardImage
+from .card.card_hearder import CardHeader
+from .card.card_footer import CardFooter
 from .breadcrumbs import QBreadcrumbs
-from .code_editor import CodeEditor
+from .code_editor.code_editor import CodeEditor
 from .title_with_actions import TitleWithActions
 from .title_block import TitleBlock
 from .clwit import CLWIT, ToggleBtn, SearchPanel
